fc26c02f3051f8dd5f5324fe8ca0a08893a5ec1b
haple <aleph.oliveira@etec.sp.gov.br>
2020-08-19 17:32:51 -0300

    Add PORT env var

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -55,5 +55,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log(`🚀 Server started on port ${process.env.PORT}!`);
+  console.log(`🚀 Server started on port ${process.env.PORT || 3333}!`);
 });
