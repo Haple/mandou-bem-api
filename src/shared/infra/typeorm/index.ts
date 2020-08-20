69fde11d6f5f92de41fda240d9c4cf785eac6938
haple <aleph.oliveira@etec.sp.gov.br>
2020-08-20 17:56:00 -0300

    Fix build configs

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,29 @@
 import { createConnections } from 'typeorm';
 
-createConnections();
+createConnections([
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DATABASE,
+    entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+    migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/shared/infra/typeorm/migrations',
+    },
+  },
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.MONGO_HOST,
+    port: parseInt(process.env.MONGO_PORT || '27017', 10),
+    username: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASS,
+    database: process.env.MONGO_DATABASE,
+    useUnifiedTopology: true,
+    entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
+  },
+]);
