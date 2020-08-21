7303701dafea29cde6c83572ed1c439557b723d9
haple <aleph.oliveira@etec.sp.gov.br>
2020-08-21 18:00:56 -0300

    Add configs to run migrations with TypeORM CLI

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+21-08-2020
+
+- Foi adicionado o arquivo ormconfig.js para conseguirmos
+executar as _migrations_ usando a CLI do TypeORM
---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+// Configs to run migrations with TypeORM CLI
+
+module.exports = [
+  {
+    "name": "default",
+    "type": "postgres",
+    "host": process.env.POSTGRES_HOST,
+    "port": process.env.POSTGRES_PORT,
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASS,
+    "database": process.env.POSTGRES_DATABASE,
+    // "ssl": {
+    //   "rejectUnauthorized": false,
+    // },
+    "entities": [
+      "./src/modules/**/infra/typeorm/entities/*.ts"
+    ],
+    "migrations": [
+      "./src/shared/infra/typeorm/migrations/*.ts"
+    ],
+    "cli": {
+      "migrationsDir": "./src/shared/infra/typeorm/migrations"
+    }
+  }
+];
