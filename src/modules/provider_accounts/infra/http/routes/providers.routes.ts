7af71979418abb26528bb72445f491e1a86dafe9
haple <aleph.oliveira@etec.sp.gov.br>
2021-09-09 17:32:09 -0300

    Fix providers routes

--- src/modules/provider_accounts/infra/http/routes/providers.routes.ts
+++ src/modules/provider_accounts/infra/http/routes/providers.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
+import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProviderAccountsController from '../controllers/ProvidersController';
 
-const providerAccountsRouter = Router();
-const providerAccountsController = new ProviderAccountsController();
+const providersRouter = Router();
+const providersController = new ProviderAccountsController();
 
-providerAccountsRouter.get('/', providerAccountsController.index);
+providersRouter.use(ensureAuthenticaded);
 
-export default providerAccountsRouter;
+providersRouter.get('/', providersController.index);
+
+export default providersRouter;
